5b1bd03a99e796315d76ebc1cb0eff5a69dd84bd
Isanne van Steen <ivansteen@student.tudelft.nl>
2025-07-03 00:09:27 +0200

    Doe gewoon meer ranks ofzo

--- server/room.py
+++ server/room.py
@@ -12,7 +12,7 @@ class Room:
 
     def init_game_state(self):
         suits = ["hearts", "diamonds", "clubs", "spades"]
-        ranks = ["7", "8", "9", "10", "J", "Q", "K", "A"]
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
         deck = [f"{rank}_of_{suit}" for suit in suits for rank in ranks]
         import random
 
