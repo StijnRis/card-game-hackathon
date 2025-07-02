9f6d5ee11bfcf29e17647084ca94cd5525e9cc40
Stijn <risseeuw.stijn@gmail.com>
2025-07-02 22:47:45 +0200

    Setup

--- types/Card.ts
+++
@@ -1,8 +0,0 @@
-export interface Card {
-    id: string;
-    symbol: string;
-    suit: string;
-    color: string;
-    type?: string; // for extensibility
-    [key: string]: any;
-}
--- types/Room.ts
+++
@@ -1,7 +0,0 @@
-import { Player } from "./Player";
-
-export interface Room {
-    code: string;
-    players: Player[];
-    gameState: any;
-}
